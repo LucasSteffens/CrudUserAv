f0ee2d42a8d235b9586e49a9338ba70f0a6bccc7
Higor <higornf2009@gmail.com>
2020-09-29 21:11:03 -0300

    Criando o model user

---
+++ av1/src/main/java/br/com/senai/av1/model/Usuario.java
@@ -0,0 +1,71 @@
+package br.com.senai.av1.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String login;
+	private String senha;
+	private String cpf;
+	
+	public Usuario() {
+		
+	}
+	
+	
+
+	public Usuario(Long id, String login, String senha, String cpf) {
+		super();
+		this.id = id;
+		this.login = login;
+		this.senha = senha;
+		this.cpf = cpf;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+	
+}
